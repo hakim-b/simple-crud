13af57cf3b9ad7946842a195f76406597a84e280
hakim-b <hakimbouabdellah02@gmail.com>
2025-12-30 18:25:24 -0500

    Implement user display logic with conditional rendering in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,12 @@ import { Button } from "~/components/ui/button";
 import { DialogContent, DialogHeader } from "~/components/ui/dialog";
 import UsersTable from "~/components/users-table";
 import UserForm from "~/components/user-form";
+import { getUsers } from "~/actions/users";
+import { Show } from "react-haiku";
 
-function Home() {
+async function Home() {
+  const allUsers = await getUsers();
+
   return (
     <div className="flex flex-col gap-4 max-w-7xl mx-auto p-4 md:p-24">
       <h1 className="text-4xl font-bold">Basic Drizzle CRUD demo</h1>
@@ -34,7 +38,15 @@ function Home() {
         </Dialog>
       </div>
 
-      <UsersTable />
+      {/* @ts-expect-error react-haiku types are not fully compatible with React server components */}
+      <Show>
+        <Show.When isTrue={allUsers.length === 0}>
+          <h2 className="text-3xl font-bold">NO USERS</h2>
+        </Show.When>
+        <Show.Else>
+          <UsersTable />
+        </Show.Else>
+      </Show>
     </div>
   );
 }
